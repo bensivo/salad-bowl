6d45e0aa7bbfad1a719f05bcbba08966923353f3
Ben Sivoravong <bensivo@gmail.com>
2023-06-28 00:43:11 -0500

    #9 Implemented UI for homepage

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,13 +1,39 @@
-import Lobby from './lobby'
-import styles from './page.module.css'
+'use client';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1>Salad Bowl</h1>
+import { useState } from 'react';
+import { CharInput } from '../components/char-input';
+import './page.css';
 
-      <Lobby></Lobby>
-     
-    </main>
-  )
+export default function HomePage() {
+
+    const [joinCode, setJoinCode] = useState('');
+
+    const onClickNewGame = () => {
+        // TODO: make a request to the server to make sure the lobby can be created
+        window.location.href = '/lobby' // TODO: add the lobby's url here
+    }
+
+    const onClickJoinGame = () => {
+        console.log('Joining Game with code', joinCode);
+    }
+
+    return (
+        <div id="homepage">
+            <div id='content-main' className='content-main card'>
+                <section id='title'>
+                    <h1>Salad Bowl</h1>
+                    <h3>by bensivo</h3>
+                </section>
+
+                <section id='menu'>
+                    <button onClick={onClickNewGame}>New Game</button>
+
+                    <div id='char-input-container'>
+                        <CharInput template="xxx-xxx" onChange={setJoinCode}></CharInput>
+                    </div>
+                    <button onClick={onClickJoinGame}>Join Game</button>
+                </section>
+            </div>
+        </div>
+    )
 }
---
+++ webapp/components/char-input.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useRef, useState } from "react";
+import './char-input.css';
+
+export interface CharInputProps {
+    // A string of the form 'xxx-xxx-xxx', where each 'x' will become an input
+    template: string;
+
+    onChange: (value: string) => void;
+}
+
+/**
+ * CharInput is an input component inspired by 2FA screens, where each
+ * character is its own input element, and typing a letter automatically 
+ * moves you to the next input
+ */
+export function CharInput(props: CharInputProps) {
+    const numValues = props.template.replace('-', '').length;
+    const initialValue = Array.from(Array(numValues)).map(_ => ''); // one-liner to generate N sized array of empty strings
+
+    const [values, setValues] = useState<string[]>(initialValue);
+    useEffect(() => {
+        props.onChange(values.join(''));
+    })
+
+    const inputRefs = useRef<Array<HTMLInputElement | null>>([]);
+
+    function focusIndex(prevIndex: number, nextIndex: number) {
+        if (nextIndex >= 0 && nextIndex <= numValues - 1) {
+            (inputRefs.current[nextIndex] as HTMLInputElement).focus()
+        } else {
+            (inputRefs.current[prevIndex] as HTMLInputElement).blur();
+        }
+    }
+
+    function onKeyDown (i: number, e: KeyboardEvent) {
+        e.preventDefault();
+
+        if (e.key === 'Backspace') {
+            const newValues = [...values]
+            newValues[i] = '';
+            setValues(newValues);
+
+            focusIndex(i, i - 1);
+        } else {
+            const newValues = [...values]
+            newValues[i] = e.key.toUpperCase();
+            setValues(newValues);
+
+            focusIndex(i, i + 1);
+        }
+    }
+
+    const inputs = values.map((value, i) => (
+        <input
+            ref={ref => inputRefs.current[i] = ref}
+            key={i}
+            type="text"
+            placeholder="X"
+            value={value}
+            onKeyDown={(e) => onKeyDown(i, e as any)}
+            onChange={(e) => { }}
+        ></input>
+    ))
+
+    const elements = []
+    let inputCounter = 0;
+    for (const char of props.template) {
+        switch (char) {
+            case 'X':
+            case 'x':
+                elements.push(inputs[inputCounter]);
+                inputCounter++;
+                break;
+            case '-':
+                elements.push(
+                    <span key={"after-" + inputCounter}>-</span>
+                )
+                break;
+            default:
+                throw new Error("Template string can only contain 'X', 'x', and '-'.")
+        }
+    }
+
+    return (
+        <div className="char-input">
+            {elements}
+        </div>
+    )
+}
